11d03200dbdd371d9a60cc2e776a7d20ff5d251c
xxyt778 <yb.shim@metanetglobal.com>
2023-06-26 11:30:21 +0900

    추가 commit

---
+++ src/components/AddExpenseForm.js
@@ -0,0 +1,204 @@
+import { useState } from "react"
+import styled from 'styled-components'
+import { Button, Col, Form, Row } from "react-bootstrap"
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { groupMembersState } from "../state/groupMembers"
+import { expensesState } from "../state/expenses"
+
+export const AddExpenseForm = () => {
+    const [validated, setValidated] = useState(false)
+    const members = useRecoilValue(groupMembersState)
+    
+    const today = new Date()
+    const [date, setDate] = useState([today.getFullYear(), today.getMonth() + 1, `0${today.getDate()}`.slice(-2)].join("-"))    //비용 년월일
+    const [desc, setDesc] = useState('')    //비용 설명
+    const [amount, setAmount] = useState(undefined)     //비용 
+    const [payer, setPayer] = useState(null)    //누가 결제
+
+    const setExpense = useSetRecoilState(expensesState)
+    const expenses = useRecoilValue(expensesState)
+    
+    const [isDescValid, setIsDescValid] = useState(false)
+    const [isPayerValid, setIsPayerValid] = useState(false)
+    const [isAmountValid, setIsAmountValid] = useState(false)
+
+    const checkValidity = () => {
+      const descValid = desc.length > 0
+      const payerValid = payer !== null
+      const amountValid = !!amount && amount > 0
+  
+      setIsDescValid(descValid)
+      setIsPayerValid(payerValid)
+      setIsAmountValid(amountValid)
+  
+      return descValid && payerValid && amountValid
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        event.stopPropagation()
+
+        if(checkValidity()){
+          const newExpense = {
+              date,
+              desc,
+              amount,
+              payer,
+          }
+          setExpense(expense => [
+              ...expense,
+              newExpense,
+          ])
+        }
+        
+        setValidated(true)
+    }
+
+    return (
+        <StyledWrapper>
+          <Form noValidate onSubmit={handleSubmit}>
+            <StyledTitle>1. 비용 추가하기</StyledTitle>
+            <Row>
+              <Col xs={12}>
+                <StyledFormGroup>
+                  <Form.Control
+                    type="date"
+                    placeholder="결제한 날짜를 선택해 주세요"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                  />
+                </StyledFormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
+                <StyledFormGroup>
+                  <Form.Control
+                    type="text"
+                    isInvalid={!isDescValid && validated}
+                    isValid={isDescValid}
+                    placeholder="비용에 대한 설명을 입력해 주세요"
+                    value={desc}
+                    onChange={({target}) => setDesc(target.value)}
+                  />
+                  <Form.Control.Feedback
+                    type="invalid"
+                    data-valid={isDescValid}
+                  >
+                    비용 내용을 입력해 주셔야 합니다.
+                  </Form.Control.Feedback>
+                </StyledFormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} lg={6}>
+                <StyledFormGroup>
+                  <Form.Control
+                    type="number"
+                    step="0.01"
+                    placeholder="비용은 얼마였나요?"
+                    min="0"
+                    value={amount}
+                    isInvalid={!isAmountValid && validated}
+                    isValid={isAmountValid}
+                    onChange={({target}) => setAmount(target.value)}
+                  />
+                  <Form.Control.Feedback
+                    data-valid={isAmountValid}
+                    type="invalid"
+                  >
+                    1원 이상의 금액을 입력해 주셔야 합니다.</Form.Control.Feedback>
+                </StyledFormGroup>
+              </Col>
+              <Col xs={12} lg={6}>
+                <StyledFormGroup>
+                  <Form.Select
+                    isValid={isPayerValid}
+                    isInvalid={!isPayerValid && validated}
+                    defaultValue=""
+                    className="form-control"
+                    onChange={({target}) => setPayer(target.value)}
+                  >
+                    <option disabled value="">누가 결제 했나요?</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>D</option>
+                    {members && members.map(member =>
+                      <option key={member} value={member}>{member}</option>
+                    )}
+                  </Form.Select>
+                  <Form.Control.Feedback
+                    data-valid={isPayerValid}
+                    type="invalid"
+                  >결제자를 선택해 주셔야 합니다.</Form.Control.Feedback>
+                </StyledFormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} className="d-grid gap-2">
+                <StyledSubmitButton>추가하기</StyledSubmitButton>
+              </Col>
+            </Row>
+          </Form>
+        </StyledWrapper>
+    )
+}
+
+const StyledWrapper = styled.div`
+  padding: 40px;
+  background-color: #683BA1;
+  box-shadow: 3px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+`
+
+const StyledFormGroup = styled(Form.Group)`
+  margin-bottom: 15px;
+
+  input, select {
+    background: #59359A;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+    border: 0;
+    color: #F8F9FA;
+    height: 40px;
+
+    &:focus {
+      color: #F8F9FA;
+      background: #59359A;
+      filter: brightness(80%);
+    }
+
+    ::placeholder {
+      color: #F8F9FA;
+    }
+  }
+`
+export const StyledTitle = styled.h3`
+  color: #FFFBFB;
+  text-align: center;
+  font-weight: 700;
+  font-size: 25px;
+  line-height: 30px;
+  letter-spacing: 0.25px;
+  margin-bottom: 15px;
+  @media screen and (max-width: 600px) {
+    font-size: 5.5vw;
+    line-height: 6vw;
+  }
+`
+const StyledSubmitButton = styled(Button).attrs({
+  type: 'submit'
+})`
+  height: 45px;
+  border: 0px;
+  border-radius: 8px;
+  background-color: #E2D9F3;
+  color: #59359A;
+  margin-top: 10px;
+
+  &:hover, &:focus {
+    background-color: #E2D9F3;
+    filter: rgba(0,0,0,0.3);
+  }
+`
--- src/components/ExpenseMain.js
+++ src/components/ExpenseMain.js
@@ -1,5 +1,81 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { AddExpenseForm } from "./AddExpenseForm"
+import { ExpenseTable } from "./ExpenseTable"
+import { useState } from "react"
+import styled from 'styled-components'
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { groupNameState } from "../state/groupName"
+import { SettlementSummary } from "./SettlementSummary"
+
+
 export const ExpenseMain = () => {
-    return (
-        <div>333</div>
+    return (       
+        <Container fluid>
+            <Row>
+                <Col xs={12} sm={5} md={4}>
+                    <LeftPane />
+                </Col>
+                <Col>
+                    <RightPane />
+                </Col>
+            </Row>
+        </Container>
     )
 }
+
+const LeftPane = () => (
+    <Container>
+        <StyledGapRow>
+            <Row>
+                <AddExpenseForm />
+            </Row>
+            <Row>
+                <SettlementSummary />
+            </Row>
+        </StyledGapRow>
+    </Container>
+)
+
+const RightPane = () => {
+    const groupName = useRecoilValue(groupNameState)
+    return (
+        <StyledRightPaneWrapper>
+            <Row>
+                <StyledGroupName>{groupName || '그룹 이름'}</StyledGroupName>
+            </Row>
+            <Row>
+                <ExpenseTable />
+            </Row>
+        </StyledRightPaneWrapper>
+    )
+}
+
+const StyledRightPaneWrapper = styled(Container)`
+  padding: 5vh 2vw 2vh 2vw;
+
+  @media screen and (max-width: 600px) {
+    padding: 50px 25px;
+  }
+`
+
+const StyledGroupName = styled.h2`
+  margin-bottom: 6vh;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 40px;
+  text-align: center;
+  @media screen and (max-width: 600px) {
+    font-size: 9vw;
+    margin-bottom: 30px;
+  }
+`
+
+const StyledGapRow = styled(Row)`
+  gap: 3vh;
+  padding-top: 14vh;
+  justify-content: center;
+
+  @media screen and (max-width: 600px) {
+    padding-top: 30px;
+  }
+`
